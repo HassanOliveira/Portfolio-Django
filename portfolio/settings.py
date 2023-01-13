a5d6c4faf43474759bf59598f55aee20132cb1ee
HassanOliveira <hassan_bittencourt@hotmail.com>
2023-01-13 22:28:46 +0100

    Sending requirements.txt updates

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,6 +149,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'hassan_bittencourt@hotmail.com'
 EMAIL_HOST_PASSWORD = 'roinhnkpahbzdqpy'
 
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
 
 # AWS_QUERYSTRING_AUTH = False
 
