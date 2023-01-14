19cc633ae78fcd795a830806a2d3bb8d3eb919e3
HassanOliveira <hassan_bittencourt@hotmail.com>
2023-01-14 15:36:27 +0100

    Updating wsgi.py

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = os.path.expanduser('~/portfolio')
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+application = StaticFilesHandler(get_wsgi_application())
+
